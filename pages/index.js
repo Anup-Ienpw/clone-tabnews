6338a65bfff6ab6c2534580cf54d34c2dfe7f5bf
Luciano Borges <luciano.pborges79@gmail.com>
2025-12-06 22:11:12 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return <h1> Hello World !!!! ;D </h1>;
+}
+
+//export default function Home() {
+//}
+
+//export function Teste2() {
+//  return <p>Outra função</p>;
+//}
+
